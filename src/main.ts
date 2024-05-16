e316cdc45ebeb53359f7b3c3dd7e44381c6290f9
sangddong <hehe74029@gmail.com>
2024-05-16 17:29:04 +0900

    chore: config 설치

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SuccessInterceptor } from './common/success-response.interceptor';
+import { GlobalExceptionFilter } from './exceptions/global-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(new SuccessInterceptor());
+  app.useGlobalFilters(new GlobalExceptionFilter());
+
   await app.listen(3000);
 }
 bootstrap();
---
+++ src/types/response.type.ts
@@ -0,0 +1,6 @@
+export type TResponse<T> = {
+  success: boolean;
+  result: T | null;
+  message: string | null;
+};
+export type TExceptionResponse = string | { message: string; error?: string };

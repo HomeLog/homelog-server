dc046a64c89b24389ea9d0f7d1211d3cdfc59a92
jimyungkoh <kojimyung1206@naver.com>
2024-05-18 17:33:46 +0900

    fix: 쿠키 옵션 secure 동적 할당
    - 로컬 환경은 http 프로토콜을 사용하므로 secure 옵션이 들어가면 쿠키 값을 제대로 저장하지 못함

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,28 +1,32 @@
 import { Controller, Delete, Get, Query, Res } from '@nestjs/common';
-import { UsersService } from './users.service';
+import { ConfigService } from '@nestjs/config';
 import { CookieOptions, Response } from 'express';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
   private readonly cookieOptions: CookieOptions;
-  constructor(private readonly usersService: UsersService) {
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly configService: ConfigService,
+  ) {
     this.cookieOptions = {
       maxAge: 1000 * 60 * 15,
       httpOnly: true,
-      secure: true,
       sameSite: 'none',
+      secure: this.configService.get('NODE_ENV') === 'production',
     };
   }
 
   @Get('kakao')
-  kakaoLogIn(@Res() response: Response) {
+  kakaoSignIn(@Res() response: Response) {
     const url = this.usersService.getKakaoCode();
     return response.redirect(url);
   }
 
   @Get('kakao/callback')
   async kakaoCallback(@Query('code') code: string, @Res() response: Response) {
-    const accessToken = await this.usersService.kakaoLogIn(code);
+    const accessToken = await this.usersService.kakaoSignIn(code);
     response.cookie('accessToken', accessToken, this.cookieOptions);
     return response.send({ accessToken });
   }

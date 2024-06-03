37fd187315affe4f9540a2c377500c10fcb5a98e
jimyungkoh <kojimyung1206@naver.com>
2024-06-03 15:54:13 +0900

    feat: 환경 변수 사용하여 CORS 및 포트 설정
    - CORS_ORIGIN 및 PORT 환경 변수를 사용하도록 수정
    - ConfigService를 사용하여 설정 값 로드

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,25 @@
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 import { SuccessInterceptor } from './common/success-response.interceptor';
 import { GlobalExceptionFilter } from './exceptions/global-exception.filter';
-import * as cookieParser from 'cookie-parser';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get<ConfigService>(ConfigService);
+
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: configService.get('CORS_ORIGIN') ?? 'http://localhost:3000',
     credentials: true,
   });
+
   app.use(cookieParser());
   app.useGlobalInterceptors(new SuccessInterceptor());
   app.useGlobalFilters(new GlobalExceptionFilter());
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
 
-  await app.listen(3001);
+  await app.listen(configService.get('PORT') ?? 3001);
 }
 bootstrap();

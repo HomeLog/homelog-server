8022ba0bdb5190777f738b688606c7c9e7610c13
jimyungkoh <kojimyung1206@naver.com>
2024-06-27 21:07:36 +0900

    feat: 방명록 페이지네이션 관련 에러 코드 추가
    - 페이지 번호 관련 에러 코드 추가 (양수, 1 이상)
    - 페이지 크기 관련 에러 코드 추가 (양수, 1 이상, 20 이하)
    - 각 에러 코드에 대한 메시지 및 상태 코드 설정

--- src/common/errors/error-codes.ts
+++ src/common/errors/error-codes.ts
@@ -5,6 +5,30 @@ export class ErrorCodes {
   ) {}
 
   // 방명록 관련 에러
+  static readonly PAGE_MUST_BE_NUMBER = new ErrorCodes(
+    'page는 양수여야 합니다.',
+    400,
+  );
+
+  static readonly PAGE_MUST_BE_AT_LEAST_ONE = new ErrorCodes(
+    'page는 1 이상이어야 합니다.',
+    400,
+  );
+
+  static readonly PAGE_LIMIT_MUST_BE_NUMBER = new ErrorCodes(
+    'size는 양수여야 합니다.',
+    400,
+  );
+
+  static readonly PAGE_LIMIT_MUST_BE_AT_LEAST_ONE = new ErrorCodes(
+    'size는 최소 1 이상이어야 합니다.',
+    400,
+  );
+
+  static readonly PAGE_LIMIT_MUST_BE_AT_MOST_TWENTY = new ErrorCodes(
+    'size는 최대 20 이하이어야 합니다.',
+    400,
+  );
   static readonly GUESTBOOK_NOT_FOUND = new ErrorCodes(
     '방명록을 찾을 수 없습니다.',
     404,

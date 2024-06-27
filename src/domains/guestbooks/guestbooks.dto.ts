1262eac2feec806bf30ecbfb432afabf3afd90f9
jimyungkoh <kojimyung1206@naver.com>
2024-06-27 21:07:04 +0900

    feat: 방명록 페이지네이션 쿼리 DTO 추가
    - PaginationQueryDto 클래스 생성
    - page와 limit 필드 추가 및 유효성 검사 설정
    - 기본값 및 최소/최대 값 제한 설정
    - 에러 메시지 연결

--- src/domains/guestbooks/guestbooks.dto.ts
+++ src/domains/guestbooks/guestbooks.dto.ts
@@ -1,4 +1,13 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { Type } from 'class-transformer';
+import {
+  IsInt,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Max,
+  Min,
+} from 'class-validator';
+import { ErrorCodes } from 'src/common/errors/error-codes';
 
 export class CreateGuestbookDto {
   @IsString()
@@ -10,4 +19,29 @@ export class UpdateGuestbookDto {
   @IsString()
   @IsNotEmpty({ message: '방명록은 최소 1자 이상이어야 합니다.' })
   content: string;
+}
+
+export class PaginationQueryDto {
+  @Type(() => Number)
+  @IsInt({
+    message: ErrorCodes.PAGE_MUST_BE_NUMBER.message,
+  })
+  @IsOptional()
+  @Min(1, {
+    message: ErrorCodes.PAGE_MUST_BE_AT_LEAST_ONE.message,
+  })
+  page?: number = 1;
+
+  @Type(() => Number)
+  @IsInt({
+    message: ErrorCodes.PAGE_LIMIT_MUST_BE_NUMBER.message,
+  })
+  @IsOptional()
+  @Min(1, {
+    message: ErrorCodes.PAGE_LIMIT_MUST_BE_AT_LEAST_ONE.message,
+  })
+  @Max(20, {
+    message: ErrorCodes.PAGE_LIMIT_MUST_BE_AT_MOST_TWENTY.message,
+  })
+  limit?: number = 10;
 }

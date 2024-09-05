5854b4874aa4bd3855985939958d3892528ef8f1
jimyungkoh <kojimyung1206@naver.com>
2024-09-05 23:26:46 +0900

    feat: 추가된 컴포넌트 레이어 유저 모듈에 등록

--- src/domains/users/users.module.ts
+++ src/domains/users/users.module.ts
@@ -1,14 +1,26 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestjsFormDataModule } from 'nestjs-form-data';
 import { ProfileImageUploadInterceptor } from 'src/common/interceptors/profile-image-upload.interceptor';
 import { StorageModule } from '../../storage/storage.module';
+import { KakaoAuthComponent } from './components/kakao-auth.component';
+import { TokenManagerComponent } from './components/token-manager.component';
+import { UsersRepository } from './components/users.repository';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService, ConfigService, ProfileImageUploadInterceptor],
-  imports: [StorageModule, NestjsFormDataModule],
+  providers: [
+    UsersService,
+    ConfigService,
+    ProfileImageUploadInterceptor,
+    KakaoAuthComponent,
+    TokenManagerComponent,
+    UsersRepository,
+  ],
+  imports: [HttpModule, StorageModule, NestjsFormDataModule],
+  exports: [UsersService, KakaoAuthComponent],
 })
 export class UsersModule {}
